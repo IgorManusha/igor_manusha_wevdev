3c8d1b1471fdb193b7b3b624b33ab3b3da5a92d7
IgorManusha <igormanushaviktor@gmail.com>
2024-11-25 21:54:18 +0200

    fetch 2

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-
-
 async function x() {
-    const response = await fetch(“https://hp-api.onrender.com/api/characters”)
-const data = await response.json()
-console.log(data)
+  const response = await fetch("https://hp-api.onrender.com/characters");
+  const data = await response.json();
+  console.log(data);
 }
 x();
